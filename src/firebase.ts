d25e362f88c920d168ce7ea4b99b89dacc37222a
vochauthanhc1trungvuong-web <vochauthanh.c1trungvuong@moet.edu.vn>
2026-03-24 21:23:36 +0700

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,14 +1,17 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBaNHC68XKFyjqqxfCOKE2XoasBVC_nuZM",
-  authDomain: "quanlylophoc-e67d2.firebaseapp.com",
-  projectId: "quanlylophoc-e67d2",
-  storageBucket: "quanlylophoc-e67d2.firebasestorage.app",
-  messagingSenderId: "554992971133",
-  appId: "1:554992971133:web:ab88402bfe23f199063014"
+  apiKey: "AIzaSyCcyYK25PJpqv8Z197OswBK9P8EByyzswI",
+  authDomain: "qllhthoihiendai.firebaseapp.com",
+  projectId: "qllhthoihiendai",
+  storageBucket: "qllhthoihiendai.firebasestorage.app",
+  messagingSenderId: "534087696793",
+  appId: "1:534087696793:web:9ae4e9a009577aab10d308"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
