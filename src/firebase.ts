bffbcc95de22ef4817c5f9a75eb67306ada6a873
vochauthanhc1trungvuong-web <vochauthanh.c1trungvuong@moet.edu.vn>
2026-03-24 21:29:17 +0700

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCcyYK25PJpqv8Z197OswBK9P8EByyzswI",
   authDomain: "qllhthoihiendai.firebaseapp.com",
@@ -12,5 +9,8 @@ const firebaseConfig = {
   messagingSenderId: "534087696793",
   appId: "1:534087696793:web:9ae4e9a009577aab10d308"
 };
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
+
